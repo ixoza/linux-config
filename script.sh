ea0dd81c56b9e8e46dc1bfde65724fb28e7280d3
artem <you@example.com>
2024-03-08 10:19:05 +0100

    full script (to test on real vm)

--- script.sh
+++ script.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
+while [[ $ip != *.*.*.* ]]; do
+    echo "Please enter a valid IP address for eth0:"
+    read ip
+done
+
+while [[ $mask -lt 1 || $mask -gt 32 ]]; do
+    echo "Please enter a valid mask for eth0:"
+    read mask
+done
+
+echo "Hostname?"
+read name
+
 apt update && apt upgrade -y
+apt autoremove
+apt clean
+apt install git curl tree wget neofetch htop tldr vim glances lsb-release unzip ufw openssh-server -y
 
-apt install git curl tree wget neofetch htop tldr -y
+hostnamectl set-hostname $name
+echo "127.0.0.1 $name" >> /etc/hosts
 
-apt list --installed
+ip addr add $ip/$mask dev eth0
+
+echo "The IP address has been set to:"
+ip addr show eth0
+echo "Hostname now is : "
+hostname
+echo "Hosts is : "
+cat /etc/hosts
+
+for i in {5..1}; do
+    echo "Rebooting in: $i..."
+    sleep 1
+done
+
+init 6
